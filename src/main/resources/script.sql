a6c82ff86362b7c4b34e4a5ead5f9e3b6d9e1d40
marcos <marcos.wp@gmail.com>
2013-04-25 23:27:55 -0300

    schema to add tag system

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -11,3 +11,23 @@ source varchar(255),
 source_label varchar(255), 
 status tinyint,
 random_factor bigint)
+
+create table tag( 
+entry_id bigint,
+id varchar(255),
+label varchar(255),
+clicks bigint,
+primary key (entry_id, id)
+)
+
+insert into tag (entry_id, id, label, clicks)
+select id, source, source_label, 0 from entry
+
+insert into tag
+select entry_id, id, label, 0 from (
+select id entry_id, 'comunidade' id, 'Comunidade' label from entry where source='cvj' or source='prefeitura' or source='defesa_civil'
+union
+select id entry_id, 'negocios' id, 'Negócios' label from entry where source='acij' or source='ajorpeme' or source='cdl_joinville'
+union
+select id entry_id, 'educacao' id, 'Educação' label from entry where source='sociesc' or source='udesc' or source='univille'
+) nt
