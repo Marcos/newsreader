a4f3bd384fee22822d9eaaaf8f7874f7ccebe448
marcos <marcos.wp@gmail.com>
2013-05-16 07:33:48 -0300

    test to find a way to remove duplicate whitespace and keep break line

--- src/main/java/com/openwp3x/reader/TextPreProcessor.java
+++ src/main/java/com/openwp3x/reader/TextPreProcessor.java
@@ -1,61 +1,71 @@
 package com.openwp3x.reader;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import junit.framework.Assert;
 
 import org.apache.commons.lang3.text.WordUtils;
 
 import antlr.StringUtils;
 
-
 public class TextPreProcessor {
 
-	public String normalizeWordWrap(String text) {
-		String normalizedText = text; 
-		normalizedText = normalizedText.replaceAll("\r", "");
-		normalizedText = normalizedText.replaceAll("( )+", " ");
-		normalizedText = normalizedText.replaceAll("\n *", "\n");
+	public static String normalizeWordWrap(String text) {
+		String normalizedText = text;
+		normalizedText = normalizedText.replaceAll("\r\\s+|\n\\s+", "\n");
+		normalizedText = normalizedText.replaceAll("\n\\s*", "\n");
+		normalizedText = normalizedText.replaceAll("\n+", "\n");
 		normalizedText = normalizedText.replaceAll("^\n", "");
 		normalizedText = normalizedText.replaceAll("\n$", "");
 		return normalizedText.trim();
 	}
-	
+
 	public static void main(String[] args) {
-		String[] tests = {
-		        "  x  ",          // [x]
-		        "  1   2   3  ",  // [1 2 3]
-		        "",               // []
-		        "   ",            // []
-		    };
-		    for (String test : tests) {
-		        System.out.format("[%s]%n",
-		            test.replaceAll("^ +| +$|( )+", "$1")
-		        );
-		    }
+		String text = "First Line\n  \n SecondLine Park\n  \n \n Third line ";
+		System.out.println(text.length());
+		text = text.replaceAll("\n\\s{1,}", "\n");
+		System.out.println(text.length());
+		System.out.println(text);
+		System.out.println("First Line\n\nSecondLine Park\n\n\nThird line ".equals(text));
 	}
-	
+
+//	public static void main2(String[] args) {
+//		String text = "1\n  \n 2.\n  \n \n 3 ";
+//		char[] chars = text.toCharArray();
+//		StringBuilder sb = new StringBuilder();
+//		for (int i = 0; i < chars.length; i++) {
+//			if (!Character.isLetterOrDigit(chars[i]) && !Character.isLetterOrDigit(chars[i - 1])) {
+//				continue;
+//			}
+//			sb.append(chars[i]);
+//		}
+//		System.out.println(sb.toString());
+//	}
+
 	public String treatSufixPattern(String text, String sufixPattern) {
-    	if(sufixPattern!=null){
-    		Pattern pattern = Pattern.compile(sufixPattern);
-    		Matcher matcher = pattern.matcher(text);
-    		if(matcher.find()){    			
-    			Integer endPosition = matcher.start();
+		if (sufixPattern != null) {
+			Pattern pattern = Pattern.compile(sufixPattern);
+			Matcher matcher = pattern.matcher(text);
+			if (matcher.find()) {
+				Integer endPosition = matcher.start();
 				text = text.substring(0, endPosition);
-    		}
-    	}
-    	return text;
+			}
+		}
+		return text;
 	}
-    
-    public String treatPrefixPattern(String text, String prefixPattern) {
-    	if(prefixPattern!=null){
-    		Pattern pattern = Pattern.compile(prefixPattern);
-    		Matcher matcher = pattern.matcher(text);
-    		if(matcher.find()){    			
-    			Integer startPosition = matcher.end();
+
+	public String treatPrefixPattern(String text, String prefixPattern) {
+		if (prefixPattern != null) {
+			Pattern pattern = Pattern.compile(prefixPattern);
+			Matcher matcher = pattern.matcher(text);
+			if (matcher.find()) {
+				Integer startPosition = matcher.end();
 				text = text.substring(startPosition);
-    		}
-    	}
-    	return text;
+			}
+		}
+		return text;
 	}
 
 	public String normalize(String title) {
@@ -64,9 +74,9 @@ public class TextPreProcessor {
 
 	public String getToken(String dirtText, String regex) {
 		final Pattern pattern = Pattern.compile(regex);
-        final Matcher matcher = pattern.matcher(dirtText);
-        matcher.find();
-        return matcher.group();
+		final Matcher matcher = pattern.matcher(dirtText);
+		matcher.find();
+		return matcher.group();
 	}
 
 }
