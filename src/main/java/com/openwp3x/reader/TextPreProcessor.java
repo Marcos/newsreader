fba29402c369cd826712fc33f5e7eb3914134f44
Marcos <marcos.wp@gmail.com>
2013-05-16 09:37:21 -0300

    adding function to normalize whitespace

--- src/main/java/com/openwp3x/reader/TextPreProcessor.java
+++ src/main/java/com/openwp3x/reader/TextPreProcessor.java
@@ -1,48 +1,21 @@
 package com.openwp3x.reader;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import junit.framework.Assert;
-
 import org.apache.commons.lang3.text.WordUtils;
-
-import antlr.StringUtils;
 
 public class TextPreProcessor {
 
-	public static String normalizeWordWrap(String text) {
-		String normalizedText = text;
-		normalizedText = normalizedText.replaceAll("\r\\s+|\n\\s+", "\n");
+	public String normalizeWordWrap(String text) {
+		String normalizedText = normalizeWhiteSpace(text);
+		normalizedText = normalizedText.replaceAll("\r\\s*", "\n");
 		normalizedText = normalizedText.replaceAll("\n\\s*", "\n");
 		normalizedText = normalizedText.replaceAll("\n+", "\n");
 		normalizedText = normalizedText.replaceAll("^\n", "");
 		normalizedText = normalizedText.replaceAll("\n$", "");
 		return normalizedText.trim();
 	}
-
-	public static void main(String[] args) {
-		String text = "First Line\n  \n SecondLine Park\n  \n \n Third line ";
-		System.out.println(text.length());
-		text = text.replaceAll("\n\\s{1,}", "\n");
-		System.out.println(text.length());
-		System.out.println(text);
-		System.out.println("First Line\n\nSecondLine Park\n\n\nThird line ".equals(text));
-	}
-
-//	public static void main2(String[] args) {
-//		String text = "1\n  \n 2.\n  \n \n 3 ";
-//		char[] chars = text.toCharArray();
-//		StringBuilder sb = new StringBuilder();
-//		for (int i = 0; i < chars.length; i++) {
-//			if (!Character.isLetterOrDigit(chars[i]) && !Character.isLetterOrDigit(chars[i - 1])) {
-//				continue;
-//			}
-//			sb.append(chars[i]);
-//		}
-//		System.out.println(sb.toString());
-//	}
 
 	public String treatSufixPattern(String text, String sufixPattern) {
 		if (sufixPattern != null) {
@@ -77,6 +50,18 @@ public class TextPreProcessor {
 		final Matcher matcher = pattern.matcher(dirtText);
 		matcher.find();
 		return matcher.group();
+	}
+
+	public String normalizeWhiteSpace(String text) {
+		StringBuilder newText = new StringBuilder();
+		for(char c : text.toCharArray()){
+			if(c==160){
+				newText.append((char)32);
+			}else{
+				newText.append(c);
+			}
+		}
+		return newText.toString();
 	}
 
 }
